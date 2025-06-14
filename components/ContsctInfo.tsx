e35ea5e2c701aaca6e6f6e22f3e2d60cc2506294
Curslo <richardkisivii@gmail.com>
2025-06-14 11:29:00 +0300

    Footer Adjustments

---
+++ components/ContsctInfo.tsx
@@ -0,0 +1,23 @@
+import { Mail, Phone, MapPin } from 'lucide-react'
+import React from 'react'
+
+export function ContsctInfo() {
+  return (
+          <div>
+            <h3 className="text-white font-semibold mb-4">Connect</h3>
+            <ul className="space-y-2">
+              <li className="flex items-center space-x-2 text-gray-400">
+                <Mail className="h-4 w-4" />
+                <span>hello@monakho.org</span>
+              </li>
+              <li className="flex items-center space-x-2 text-gray-400">
+                <Phone className="h-4 w-4" />
+                <span>+254 711 547617</span>
+              </li>
+              <li className="flex items-center space-x-2 text-gray-400">
+                <MapPin className="h-4 w-4" />
+                <span>Online & Everywhere</span>
+              </li>
+            </ul>
+          </div>  )
+}
