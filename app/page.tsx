d1fa496781a2aedf1da1835aa30b41fa60b78dec
Curslo <richardkisivii@gmail.com>
2025-07-06 01:06:37 +0300

    Optimizing for succesful build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
 import { Hero } from "@/components/Hero";
 import { Mission } from "@/components/Mission";
 import { Events } from "@/components/Events";
 import { Connect } from "@/components/Connect";
 import Head from "next/head";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -19,7 +17,6 @@ export default function Home() {
       <div className="pt-16">
         {/* Hero Section */}
         <Hero />
-        <Button onClick={() => payWithMpesa("254700000000", 100)}>Pay with M-Pesa</Button>
 
         {/* Mission Section */}
         <Mission />
@@ -32,17 +29,4 @@ export default function Home() {
       </div>
     </>
   );
-}
-
-async function payWithMpesa(phone: string, amount: number) {
-  const res = await fetch("/api/payments/mpesa", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ phoneNumber: phone, amount }),
-  });
-
-  const data = await res.json();
-  console.log(data);
 }
